a7d269aa0c6cf83de58b7f40e6eb9e7d49d41be4
OussemaJridi <oussemajridi78@gmail.com>
2025-12-01 17:00:39 +0100

    Adding Validations

--- src/Application/DTOs/UpdateSystemePartenaireDto.cs
+++ src/Application/DTOs/UpdateSystemePartenaireDto.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs;
 
-    public class UpdateSystemePartenaireDto
+    public class UpdateSystemePartenaireDto : IValidatableObject
     {
         [Required(ErrorMessage = "L'identifiant est requis.")]
         public Guid Id { get; set; }
@@ -10,6 +11,7 @@ namespace Application.DTOs;
         [StringLength(200, ErrorMessage = "Le nom ne peut pas dépasser 200 caractères.")]
         public string Nom { get; set; } = string.Empty;
         [Required(ErrorMessage = "Le UrlBase est requis.")]
+        [Url(ErrorMessage = "L'URL doit être valide.")]
         [StringLength(200, ErrorMessage = "Le nom ne peut pas dépasser 200 caractères.")]
         public string UrlBase { get; set; } = string.Empty;
         public string? UsernameAppel { get; set; }   
@@ -20,5 +22,20 @@ namespace Application.DTOs;
         public bool EstAppele { get; set; } = false;
         public bool EstActif { get; set; } = false;
         public DateTime DateCreation { get; set; }  = DateTime.Now;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrWhiteSpace(UrlBase))
+            {
+                if (!Uri.TryCreate(UrlBase, UriKind.Absolute, out var uri) ||
+                    (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps))
+                {
+                    yield return new ValidationResult(
+                        "L'URL de base doit être une URL http(s) absolue.",
+                        new[] { nameof(UrlBase) }
+                    );
+                }
+            }
+        }
     }
 
